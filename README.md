5cb4a241292562393fdfee9eecdff94907257657
sklarqui <sklarqui@purdue.edu>
2017-06-05 15:02:24 -0400

    added growing letters

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # People Factory
+shift comand J 
+## Day 1 HW
+* Add another input to the form
+* use the values from both inputs in the 'h1'
+##Super mega bonus Credit
+* add an empty paragraph to the page
+* Use the form values to do something with that paragraph.
+
+###Super Mega bonus credit Hyper Fighting
+* Change the apperance of the paragraph (think css) based on a value from the form.
--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const personForm =document.querySelector('form#personForm')
-
+//const buttoner=document.querySelector('button#pesonF');
+let g=20;
 function handleSubmit(ev) {
 ev.preventDefault()
 const f=ev.target
 console.log(f.personName.value)
 console.log('SUBMIT')
-const name=f.personName.value
+const name=f.personName.value+" "+f.personLastName.value
 document.querySelector('h1').textContent=name
+document.querySelector('#words').textContent=name;
+document.querySelector('#words').style.fontSize=g%100+10+'px';
+g=g+19
 }
 
 personForm.addEventListener('submit', handleSubmit)
+
+//personForm.addEventListener('submit', changer)
+//buttoner.addEventListner('submit', handleSubmit)
