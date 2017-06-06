22e451086d0209874ad1e5ae751f1c4f9e4a86a1
sklarqui <sklarqui@purdue.edu>
2017-06-06 10:13:29 -0400

    Show all form values in details

--- index.js
+++ index.js
@@ -16,14 +16,22 @@ document.querySelector('#words').style.fontSize=(20*g)%100+10+'px'
 document.querySelector('#words').style.fontStyle=sty[g%3]
 document.querySelector('#words').style.fontFamily=fam[g%5]
 const details=document.querySelector('#details')
+const favoriteColor =f.favoriteColor.value
+const age=f.age.value
 //details.innerHTML='<em>'+name+'</em>'
 //details.innerHTML =`<em>${name}</em>`
 // ctr / -- will comment out a line
-const em = document.createElement('em')
-em.textContent=name
-details.appendChild(em)
+//const em = document.createElement('em')
+//em.textContent=name
+//details.appendChild(em)
 g=g+1
-
+details.innerHTML=`
+<ul>
+<li>Name: ${name}</li>
+<li>Favorite Color: ${favoriteColor}</li>
+<li>Age: ${age}</li>
+</ul>
+`
 
 }
 
